ee2ac5299b705085bb2d2c21206414b43091739d
Will <willianwed98@gmail.com>
2020-07-20 16:01:14 -0300

    Product deleteById method

--- src/main/java/com/willian/springproduct/services/ProductService.java
+++ src/main/java/com/willian/springproduct/services/ProductService.java
@@ -3,11 +3,16 @@ package com.willian.springproduct.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.willian.springproduct.domain.Product;
 import com.willian.springproduct.repositories.ProductRepository;
+import com.willian.springproduct.services.exception.DataBaseException;
 import com.willian.springproduct.services.exception.ObjectNotFoundException;
 
 @Service
@@ -27,5 +32,20 @@ public class ProductService {
 	
 	public Product insert(Product obj) {
 			return repository.save(obj);			
+	}
+	
+	public void deleteById(Long id) {
+		try {
+			repository.deleteById(id);			
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ObjectNotFoundException(id);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ObjectNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
 	}
 }
