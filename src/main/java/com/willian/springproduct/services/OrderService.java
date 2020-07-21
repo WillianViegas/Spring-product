19b8af4931583aeb0fbbbc7b42cf9c9903e61cc1
Will <willianwed98@gmail.com>
2020-07-21 13:17:33 -0300

    Order insert and deleteById methods

--- src/main/java/com/willian/springproduct/services/OrderService.java
+++ src/main/java/com/willian/springproduct/services/OrderService.java
@@ -3,11 +3,16 @@ package com.willian.springproduct.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.willian.springproduct.domain.Order;
 import com.willian.springproduct.repositories.OrderRepository;
+import com.willian.springproduct.services.exception.DataBaseException;
 import com.willian.springproduct.services.exception.ObjectNotFoundException;
 
 @Service
@@ -23,5 +28,24 @@ public class OrderService {
 	public Order findById(Long id) {
 		Optional<Order> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(id));
+	}
+	
+	public Order insert(Order obj) {
+		return repository.save(obj);
+	}
+	
+	public void deleteById(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ObjectNotFoundException(id);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ObjectNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
 	}
 }
